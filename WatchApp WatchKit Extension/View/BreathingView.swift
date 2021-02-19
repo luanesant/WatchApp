cb0f91b105889435746fc4111689f710316a29ef
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2021-02-19 10:35:15 -0400

    Picker pra selecionar o tempo de respiração implementado e navigação para a view seguinte

---
+++ WatchApp WatchKit Extension/View/BreathingView.swift
@@ -0,0 +1,20 @@
+//
+//  BreathingView.swift
+//  WatchApp WatchKit Extension
+//
+//  Created by Vinícius de Oliveira on 19/02/21.
+//
+
+import SwiftUI
+
+struct BreathingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BreathingView_Previews: PreviewProvider {
+    static var previews: some View {
+        BreathingView()
+    }
+}
---
+++ WatchApp WatchKit Extension/View/TimeToBreathView.swift
@@ -0,0 +1,20 @@
+//
+//  TimeToBreathView.swift
+//  WatchApp WatchKit Extension
+//
+//  Created by Vinícius de Oliveira on 19/02/21.
+//
+
+import SwiftUI
+
+struct TimeToBreathView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimeToBreathView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeToBreathView()
+    }
+}
