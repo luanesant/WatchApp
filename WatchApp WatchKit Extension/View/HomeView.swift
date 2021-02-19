3ebd9b1345b3f825df7688b3ec8dfd7dbbb6248a
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2021-02-19 10:53:42 -0400

    Refatoração da HomeView

---
+++ WatchApp WatchKit Extension/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  WatchApp WatchKit Extension
+//
+//  Created by Vinícius de Oliveira on 19/02/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
