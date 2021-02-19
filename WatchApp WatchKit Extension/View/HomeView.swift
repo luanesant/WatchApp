e35f20c1ced315ceef2761bda216e41b1360e09f
Luane dos Santos Lopes <luanesantos1206@gmail.com>
2021-02-19 16:40:20 -0400

    Feat: Create Rounded

--- WatchApp WatchKit Extension/View/HomeView.swift
+++ WatchApp WatchKit Extension/View/HomeView.swift
@@ -6,28 +6,36 @@
 //
 
 import SwiftUI
+import UIKit
+import WatchKit
 
 struct HomeView: View {
+    
+    
     var body: some View {
+        
         VStack{
-            Image(systemName: "heart").padding(.bottom, 50)
+            Image(systemName: "heart").padding(.bottom, 20).padding(.top,0)
             
-        List {
-            ForEach (0..<2) {item in
-                switch item {
-                case 0:
-                    HStack{
-                        TableViewButton(iconImage: "lungs.fill", buttonTitle: "Breath")
-                        NavigationLink("",destination: TimeToBreathView())
-                            
+                List(){
+                ForEach (0..<2) {item in
+                    switch item {
+                    case 0:
+                        HStack{
+                            TableViewButton(iconImage: "lungs.fill", buttonTitle: "Breath")
+                            NavigationLink("",destination: TimeToBreathView())
+                        }
+                    case 1:
+                        TableViewButton(iconImage: "eyebrow", buttonTitle: "5 senses")
+                    default:
+                        TableViewButton(iconImage: "", buttonTitle: "")
                     }
-                case 1:
-                    TableViewButton(iconImage: "eyebrow", buttonTitle: "5 senses")
-                default:
-                    TableViewButton(iconImage: "", buttonTitle: "")
-                }
-                }
-            }
+                    
+                }.frame(height: 50.0).hidden()//.listRowBackground(Color.pink)
+                    
+                    
+                }.clipShape(RoundedRectangle(cornerSize: CGSize(width: 24, height: 1200), style: .circular)).frame(height: 120, alignment: .center)
+           
         }
     }
 }
@@ -51,3 +59,4 @@ struct TableViewButton: View {
         }
     }
 }
+
