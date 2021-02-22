a91d027b331d8b05a9e208883dd8fc1e0f55796a
Luane dos Santos Lopes <luanesantos1206@gmail.com>
2021-02-22 16:30:10 -0400

    Feat: Trying create FeedBackView

--- WatchApp WatchKit Extension/View/FeedBack.swift
+++ WatchApp WatchKit Extension/View/FeedBack.swift
@@ -11,12 +11,29 @@ struct FeedBack: View {
     var body: some View {
         ScrollView(.horizontal){
             HStack{
-                Image(systemName: "return").frame(minWidth: 80, minHeight: 80).background(Color.gray).clipShape(Circle())
-                Image(systemName: "return").frame(minWidth: 80, minHeight: 80).background(Color.gray).clipShape(Circle())
-                Image(systemName: "return").frame(minWidth: 80, minHeight: 80).background(Color.gray).clipShape(Circle())
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "return").frame(minWidth: 80, minHeight: 80)//background(Color.gray).clipShape(Circle())
+                }).clipShape(Circle(), style: FillStyle()).background(Color.clear).buttonStyle(BorderedButtonStyle(tint: .blue))
                 
-               }.listStyle(CarouselListStyle())
-            }
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "return").frame(minWidth: 80, minHeight: 80)//background(Color.gray).clipShape(Circle())
+                }).clipShape(Circle(), style: FillStyle()).background(Color.clear).buttonStyle(BorderedButtonStyle(tint: .blue))
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "return").frame(minWidth: 80, minHeight: 80)//background(Color.gray).clipShape(Circle())
+                }).clipShape(Circle(), style: FillStyle()).background(Color.clear).buttonStyle(BorderedButtonStyle(tint: .blue))
+                
+//                Image(systemName: "return").frame(minWidth: 80, minHeight: 80).background(Color.gray).clipShape(Circle())
+//                Image(systemName: "return").frame(minWidth: 80, minHeight: 80).background(Color.gray).clipShape(Circle())
+                
+            }.padding().listStyle(CarouselListStyle())
+        }
     }}
 
 struct FeedBack_Previews: PreviewProvider {
