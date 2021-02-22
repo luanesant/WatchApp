835714a096e310ee780e5836d4c61dbb307dee51
Luane dos Santos Lopes <luanesantos1206@gmail.com>
2021-02-22 08:58:11 -0400

    Feat: HomeView Ending

--- WatchApp WatchKit Extension/View/HomeView.swift
+++ WatchApp WatchKit Extension/View/HomeView.swift
@@ -15,53 +15,45 @@ struct HomeView: View {
         
         VStack{
             Image(systemName: "heart").padding(.bottom, 20).padding(.top,0)
-//            ZStack{
-//                List{
-//                ForEach (0..<2) {item in
-//                    switch item {
-//                    case 0:
-//                        HStack{
-//                            TableViewButton(iconImage: "lungs.fill", buttonTitle: "Breath")
-//                            NavigationLink("",destination: FeedBack())
-//                        }
-//
-//                    case 1:
-//                        TableViewButton(iconImage: "eyebrow", buttonTitle: "5 senses")
-//                        NavigationLink("",destination: Text("hey"))
-//                    default:
-//                        TableViewButton(iconImage: "", buttonTitle: "")
-//                    }
-//
-//                }.frame(height: 50.0).listRowBackground(Color.pink)
-//
-//
-//                }.clipShape(RoundedRectangle(cornerSize: CGSize(width: 24, height: 1200), style: .circular)).listStyle(CarouselListStyle())
-//            Divider()
-//            Divider()
-//            }
+            ZStack{
+                VStack{
 
-//        }//.listStyle(CarouselListStyle())
-            Spacer()
-            List {
                 NavigationLink(destination: FeedBack(), label: {
-                    TableViewButton(iconImage: "lungs.fill", buttonTitle: "Breath")
-                        .frame(width: 140, height: 44)
-                })
-                .frame(height: 50.0)
-                .listRowBackground(Color.blue)
-        
-                NavigationLink(destination: FiveSensesView(), label: {
-                    TableViewButton(iconImage: "eyebrow", buttonTitle: "5 senses")
-                        .frame(width: 140, height: 44)
-                })
-                .frame(height: 50.0)
-                .listRowBackground(Color.blue)
+                    HStack{
+                    Image(systemName: "lungs.fill")
+                        .padding(.leading,10)//.font(.title)
+                    Spacer()
+                    Text("Breath")
+                    Spacer()
+                }.foregroundColor(.white)
+
+                }
+
+                ).buttonStyle(PlainButtonStyle()).frame(height: 50.0).clipShape(Rectangle(), style: FillStyle()).background(Color.blue).foregroundColor(.white)
+
+                NavigationLink(destination: Text("hey"), label: {
+                    HStack{
+                        Image(systemName: "eyebrow")
+                            .padding(.leading,13)
+                    Spacer()
+                    Text("5 Senses")
+                    Spacer()
+                }.foregroundColor(.white)
+
+                }
+
+                ).buttonStyle(PlainButtonStyle()).frame(height: 50.0).clipShape(Rectangle(), style: FillStyle()).background(Color.blue).foregroundColor(.white)
+
+            }.clipShape(RoundedRectangle(cornerSize: CGSize(width: 24, height: 1200), style: .circular))
+            Divider()
+
             }
-            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 24, height: 1200), style: .circular))
-            .listStyle(CarouselListStyle())
+
         }
     }
+    
 }
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
@@ -69,20 +61,4 @@ struct HomeView_Previews: PreviewProvider {
     }
 }
 
-struct TableViewButton: View {
-    var iconImage: String
-    var buttonTitle: String
-    
-    var body: some View {
-        HStack(alignment: .center, spacing: 0, content: {
-            Image(systemName: iconImage)
-                .frame(width: 25, height: 25, alignment: .center)
-                .padding(.trailing, 20)
-                .padding(.leading, 20)
-            Text(buttonTitle)
-                .multilineTextAlignment(.leading)
-            Spacer()
-        })
-    }
-}
 
