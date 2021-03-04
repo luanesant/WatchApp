eec479388af514948e2fe4c7e4d4aba4ada4ee1c
Radija Maria Praia Guimarães <radija.praia@gmail.com>
2021-03-04 08:52:33 -0400

    bug fix, new modal and text correction

--- WatchApp WatchKit Extension/View/HomeView.swift
+++ WatchApp WatchKit Extension/View/HomeView.swift
@@ -42,7 +42,7 @@ struct HomeButton: View {
     var body: some View {
         HStack{
             Image(image)
-                .padding(.leading,10)
+                .padding(.leading,10).font(.body)
             Spacer()
             Text(text)
             Spacer()
