bda4230d962e3da9dea0d0486b97fa504c302a25
Luane dos Santos Lopes <luanesantos1206@gmail.com>
2021-03-05 11:45:11 -0400

    Accessibility VoiceOver Correto

--- WatchApp WatchKit Extension/View/HomeView.swift
+++ WatchApp WatchKit Extension/View/HomeView.swift
@@ -20,7 +20,7 @@ struct HomeView: View {
 //                .padding(.bottom, 20)
 //                .padding(.top,0).accessibility(label: Text(Translations.VoiceOver.heartOver))
             
-            HeartRateView()
+       //     HeartRateView()
 
             Button(action: {
                 changeView = true
@@ -30,17 +30,15 @@ struct HomeView: View {
                     Spacer()
                     Text(Translations.Titles.breath).foregroundColor(.white)
                     Spacer()
-                }
-                
-            }).buttonStyle(BorderedButtonStyle(tint: bgButton.opacity(200))).background(NavigationLink("", destination: TimeToBreathView(), isActive: $changeView))
-            .accessibility(label: Text(Translations.VoiceOver.breathOver)).buttonStyle(BorderedButtonStyle(tint: bgButton.opacity(200))).font(.body)
+                }.accessibility(label: Text(Translations.VoiceOver.breathOver))
+            }).accessibility(addTraits: .isButton).buttonStyle(BorderedButtonStyle(tint: bgButton.opacity(200))).background(NavigationLink("", destination: TimeToBreathView(), isActive: $changeView))
+            .buttonStyle(BorderedButtonStyle(tint: bgButton.opacity(200))).font(.body)
 
             NavigationLink(destination: FiveSensesView(showModalView: !UserDefaults.standard.bool(forKey: "hideModal"))) {
                 HomeButton(image: "eye", text: Translations.Titles.fiveSenses).foregroundColor(.white)
-            }.accessibility(label: Text(Translations.VoiceOver.sensesOver))
-//            .buttonStyle(BorderedButtonStyle(tint: bgButton.opacity(200))).font(.body)
+            }.accessibility(label: Text(Translations.VoiceOver.sensesOver))           .buttonStyle(BorderedButtonStyle(tint: bgButton.opacity(200))).font(.body)
                         
-        }.accessibility(label: Text("App Harmon"))
+        }
         .navigationBarBackButtonHidden(true)
         .navigationBarTitle(Translations.Titles.nameApp)
     }
