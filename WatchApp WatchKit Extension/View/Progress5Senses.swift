ed5a77188217660ef2720669c3a5a83c7dfbb007
Luane dos Santos Lopes <luanesantos1206@gmail.com>
2021-03-10 09:09:48 -0400

    Feat: Not Rectangle

--- WatchApp WatchKit Extension/View/Progress5Senses.swift
+++ WatchApp WatchKit Extension/View/Progress5Senses.swift
@@ -49,8 +49,7 @@ struct Progress5Senses: View {
                     .aspectRatio(contentMode: .fit)
                     .accessibility(label: Text(Translations.VoiceOver.imageOver))
             }
-            RoundedRectangle(cornerRadius: 30).fill(Color.red).frame(width: 10, height: 100).padding(.leading, 150.0)//.hidden()
-
+           
         }
         .frame(width: 200, height: 90, alignment: .topLeading)
         
